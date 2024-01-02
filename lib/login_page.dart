d456f9698836d582c0af2b1c389e3818ff56af82
LailyAuliaRahmi <lailyauliarahmi@webmail.umm.ac.id>
2024-01-02 18:57:54 +0700

    tambah

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/controller/auth.dart';
 import 'package:flutter_application_1/register_page.dart';
-import 'package:flutter_application_1/home_page.dart'; // Sesuaikan import dengan struktur proyek Anda
+import 'package:flutter_application_1/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -27,111 +27,116 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
       backgroundColor: Colors.indigo[200],
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                'assets/image/3.jpg', // Ganti dengan path/logo sesuai kebutuhan Anda
-                height: 150,
-                width: 150,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'assets/image/3.jpg', // Ganti dengan path/logo sesuai kebutuhan Anda
+                  height: 150,
+                  width: 150,
+                ),
               ),
-            ),
-            SizedBox(height: 20), // Jarak antara gambar dan kotak teks
-            Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.8),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(labelText: 'Email'),
-                  ),
-                  TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(labelText: 'Password'),
-                  ),
-                  SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: isLoading
-                        ? null
-                        : () async {
-                            setState(() {
-                              isLoading = true;
-                            });
-                            bool authResult = await _authController.loginUser(
-                              _emailController.text,
-                              _passwordController.text,
-                            );
-                            if (authResult) {
-                              int lastLevelCompleted =
-                                  3; // Ganti dengan logic Anda
-                              bool isLoggedIn = true; // Ganti dengan logic Anda
-                              if (isLoggedIn && lastLevelCompleted == 3) {
-                                Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        HomePage(level: lastLevelCompleted),
+              SizedBox(height: 20), // Jarak antara gambar dan kotak teks
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(labelText: 'Email'),
+                    ),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(labelText: 'Password'),
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: isLoading
+                          ? null
+                          : () async {
+                              setState(() {
+                                isLoading = true;
+                              });
+                              bool authResult = await _authController.loginUser(
+                                _emailController.text,
+                                _passwordController.text,
+                              );
+                              if (authResult) {
+                                int lastLevelCompleted =
+                                    3; // Ganti dengan logic Anda
+                                bool isLoggedIn =
+                                    true; // Ganti dengan logic Anda
+                                if (isLoggedIn && lastLevelCompleted == 3) {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomePage(level: lastLevelCompleted),
+                                    ),
+                                  );
+                                } else {
+                                  Navigator.pop(context);
+                                }
+                              } else {
+                                setState(() {
+                                  isLoading = false;
+                                });
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: Text('Login Failed'),
+                                    content: Text('Invalid email or password'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text('OK'),
+                                      ),
+                                    ],
                                   ),
                                 );
-                              } else {
-                                Navigator.pop(context);
                               }
-                            } else {
-                              setState(() {
-                                isLoading = false;
-                              });
-                              showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  title: Text('Login Failed'),
-                                  content: Text('Invalid email or password'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text('OK'),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            }
-                          },
-                    child:
-                        isLoading ? CircularProgressIndicator() : Text('Login'),
-                    style: ButtonStyle(
-                      fixedSize: MaterialStateProperty.all<Size>(
-                        Size(double.maxFinite, 40),
+                            },
+                      child: isLoading
+                          ? CircularProgressIndicator()
+                          : Text('Login'),
+                      style: ButtonStyle(
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          Size(double.maxFinite, 40),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 16),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => RegisterPage()),
-                      );
-                    },
-                    child: Text(
-                      'Don\'t have an account? Register here',
-                      style: TextStyle(color: Colors.indigo),
+                    SizedBox(height: 16),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterPage()),
+                        );
+                      },
+                      child: Text(
+                        'Don\'t have an account? Register here',
+                        style: TextStyle(color: Colors.indigo),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
