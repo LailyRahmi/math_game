2ba9a1b632aa7c72ab29610ec8fa6169a49407a4
LailyAuliaRahmi <lailyauliarahmi@webmail.umm.ac.id>
2024-01-02 19:04:16 +0700

    tambah

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -26,94 +26,96 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
       ),
       backgroundColor: Colors.indigo[200],
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                'assets/image/2.jpg', // Ganti dengan path/logo sesuai kebutuhan Anda
-                height: 150,
-                width: 150,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'assets/image/2.jpg', // Ganti dengan path/logo sesuai kebutuhan Anda
+                  height: 150,
+                  width: 150,
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.8),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(labelText: 'Email'),
-                  ),
-                  TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(labelText: 'Password'),
-                  ),
-                  SizedBox(height: 16),
-                  ElevatedButton(
-                    onPressed: isLoading
-                        ? null
-                        : () async {
-                            setState(() {
-                              isLoading = true;
-                            });
-                            bool authResult =
-                                await _authController.registerUser(
-                              _emailController.text,
-                              _passwordController.text,
-                            );
-                            if (authResult) {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => LoginPage(),
-                                ),
-                              );
-                            } else {
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(labelText: 'Email'),
+                    ),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(labelText: 'Password'),
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: isLoading
+                          ? null
+                          : () async {
                               setState(() {
-                                isLoading = false;
+                                isLoading = true;
                               });
-                              // Handle registration error, misalnya tampilkan pesan kesalahan
-                              showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  title: Text('Registration Failed'),
-                                  content: Text('Please try again later'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                      child: Text('OK'),
-                                    ),
-                                  ],
-                                ),
+                              bool authResult =
+                                  await _authController.registerUser(
+                                _emailController.text,
+                                _passwordController.text,
                               );
-                            }
-                          },
-                    child: isLoading
-                        ? CircularProgressIndicator()
-                        : Text('Register'),
-                    style: ButtonStyle(
-                      fixedSize: MaterialStateProperty.all<Size>(
-                        Size(double.maxFinite, 40),
+                              if (authResult) {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => LoginPage(),
+                                  ),
+                                );
+                              } else {
+                                setState(() {
+                                  isLoading = false;
+                                });
+                                // Handle registration error, misalnya tampilkan pesan kesalahan
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                    title: Text('Registration Failed'),
+                                    content: Text('Please try again later'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text('OK'),
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              }
+                            },
+                      child: isLoading
+                          ? CircularProgressIndicator()
+                          : Text('Register'),
+                      style: ButtonStyle(
+                        fixedSize: MaterialStateProperty.all<Size>(
+                          Size(double.maxFinite, 40),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 16),
-                ],
+                    SizedBox(height: 16),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
